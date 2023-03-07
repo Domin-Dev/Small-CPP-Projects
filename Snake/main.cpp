061fb101e2f1feb07e980d0d9c555468276ba908
Domin-Dev <domin.gamedev@gmail.com>
2023-03-07 23:43:43 +0100

    Snake 0.1

--- Snake/main.cpp
+++ Snake/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
 
 using namespace std;
 
@@ -15,25 +19,46 @@ public:
         x = posX;
         y = posY;
     }
-};
 
+    public: string ToString()
+    {
+        return to_string(x) + " " + to_string(y);
+    }
+
+    public: void AddVector(Vector2 vector2)
+    {
+        x = x + vector2.x;
+        y = y + vector2.y;
+    }
+
+    public: bool Compare(Vector2 vector2)
+    {
+       return (x == vector2.x && y == vector2.y);
+    }
+};
 
 HANDLE hConsole;
 bool isMenu;
-
 int height,width;
-int fruitX,fruitY;
+int score;
+Vector2 playerPos(0,0);
+Vector2 fruitPos(0,0);
+Vector2 direction(1,0);
 
-int spawnX,spawnY;
-Vector2 spawnPos(0,0);
-
-
-
+void Restart()
+{
+    score = 0;
+    playerPos = Vector2(width/2,height/2);
+    fruitPos = Vector2(rand() % (width - 2) + 1,rand() % (height - 2) + 1);
+}
 void SetUp()
 {
-    height = 21;
-    width = 41;
-    spawnPos = Vector2(width/2,height/2);
+    score = 0;
+    height = 20;
+    width = 30;
+    srand(time(nullptr));
+    playerPos = Vector2(width/2,height/2);
+    fruitPos = Vector2(rand() % (width - 2) + 1,rand() % (height - 2) + 1);
 
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     isMenu = true;
@@ -65,47 +90,75 @@ void GetKeys()
     {
         switch(getch())
         {
-            case 'a': break;
-
-
+            case 'a': direction = Vector2(-1,0);
+                break;
+            case 'd': direction = Vector2(1,0);
+                break;
+            case 's': direction = Vector2(0,1);
+                break;
+            case 'w': direction = Vector2(0,-1);
+                break;
         }
-
     }
 
 
 }
 
+void CheckHitBoxes()
+{
+    if(playerPos.Compare(fruitPos))
+    {
+        score ++;
+        fruitPos = Vector2(rand() % (width - 2) + 1,rand() % (height - 2) + 1);
+    }
+
+    if(playerPos.x > width || playerPos.x < 0 || playerPos.y < 0 || playerPos.y > height)
+    {
+        Restart();
+    }
+}
+
 void UpdateFrame()
 {
     system("cls");
+    playerPos.AddVector(direction);
+    CheckHitBoxes();
+
     SetConsoleTextAttribute(hConsole, 4);
-    cout << spawnPos.x << " " << spawnPos.y;
-    for(int i = 0; i < height ;i++)
+    cout << "          SCORE: " << score;
+    for(int i = 0; i < height + 3;i++)
     {
-
-
         cout << "\n";
-        for(int j = 0; j< width;j++)
+        for(int j = 0; j< width + 5;j++)
         {
-            if(spawnPos.x == j && spawnPos.y == i)
+            SetConsoleTextAttribute(hConsole, 68);
+            if(playerPos.x == j - 2 && playerPos.y == i - 1)
             {
-                SetConsoleTextAttribute(hConsole, 3);
-                cout << "@";
+               SetConsoleTextAttribute(hConsole, 2);
+               cout << "O";
+               SetConsoleTextAttribute(hConsole, 4);
+            }
+            else if(fruitPos.x == j - 2 && fruitPos.y == i - 1)
+            {
+               SetConsoleTextAttribute(hConsole, 2);
+                cout << "$";
                 SetConsoleTextAttribute(hConsole, 4);
             }
             else
             {
-                if((i == 0 || i == height -1) || (j == 0 || j == width -1))
+                if((i == 0 || i == height + 2) || (j <= 1 || j >= width + 3))
                 {
                     cout << "#";
-                    if(isMenu)Sleep(5);
+                //    if(isMenu)Sleep(5);
                 }
                 else
                 {
+                    SetConsoleTextAttribute(hConsole,0);
                     cout << " ";
                 }
             }
         }
+        SetConsoleTextAttribute(hConsole,0);
     }
 
 }
@@ -125,8 +178,8 @@ int main()
         }
         else
         {
+            GetKeys();
             UpdateFrame();
-            break;
             Sleep(10);
         }
 
